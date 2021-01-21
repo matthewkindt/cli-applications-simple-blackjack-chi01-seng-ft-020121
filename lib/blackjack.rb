af34dc5f7dbadb3d42cc741dd557173a381d9efd
Matthew Kindt <matthewkindt96@yahoo.com>
2021-01-21 19:18:22 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -8,8 +8,8 @@ def deal_card
   # code #deal_card here
 end
 
-def display_card_total
-  puts "Your cards add up to #{card_total}"
+def display_card_total(card_total)
+  puts "#{card_total}"
   #code #display_card_total here
 end
 
